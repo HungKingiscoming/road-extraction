d9ba8668a04f401aa5446cd8ca46350b29eea904
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-09-06 21:07:59 +0700

    Update __init__.py

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -1,7 +1,14 @@
-from .decoder import RoadReconstructionDecoder, RoadSegCenterlineTverskyLoss
+from .decoder import (
+    DeformableConvBlock,
+    DeformableRoadRefineBlock,
+    RoadReconstructionDecoder,
+    RoadSegCenterlineTverskyLoss,
+)
 from .model import DualBranchRoadNet, build_model
 
 __all__ = (
+    "DeformableConvBlock",
+    "DeformableRoadRefineBlock",
     "DualBranchRoadNet",
     "RoadReconstructionDecoder",
     "RoadSegCenterlineTverskyLoss",
